5c4a777b037a5bf1d4fde9fd152eb7ee8d17503f
Anthoni Marie <contact@anthoni-marie.fr>
2021-06-27 16:33:24 +0200

    [+] - Edited orga desc zone

--- frontend/src/components/OrganizationDescZone.js
+++ frontend/src/components/OrganizationDescZone.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class OrganizationDescZone extends Component {
     render() {
@@ -8,10 +9,27 @@ class OrganizationDescZone extends Component {
             <>
                 <div className="text-center">
                     <h2>{data.name}</h2>
+                    <h5>Location: {data.location}</h5>
+                    <h5>Manager E-mail: {data.email}</h5>
                 </div>
                 <div className="container-fluid">
                     <div className="row d-flex justify-content-center">
                         <div className="col-md-8">
+                            <Button
+                                href={
+                                    "mailto:" +
+                                    data.email +
+                                    "?subject=I want to join your organization - Epinaute.org&body=Hello, how could I join your organization ? Thank you!"
+                                }
+                                variant="warning"
+                                style={{
+                                    justifyContent: "center",
+                                    marginTop: "-190px",
+                                    color: "#FFFFFF",
+                                }}
+                            >
+                                I want to join
+                            </Button>{" "}
                             <p className="text">{data.description}</p>
                         </div>
                     </div>
